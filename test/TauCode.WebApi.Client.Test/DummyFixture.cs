00932ac7c3dfc7b113dc5c783602734f6ddbe2ce
Andrii Kovalenko <Andrii@invictusgurus.com>
2019-07-23 12:24:33 +0300

    dummy fixture added

---
+++ test/TauCode.WebApi.Client.Test/DummyFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.WebApi.Client.Test
+{
+    [TestFixture]
+    public class DummyFixture
+    {
+        [Test]
+        public void Dummy()
+        {
+            Assert.Pass("todo: remove when real tests are ready.");
+        }
+    }
+}
